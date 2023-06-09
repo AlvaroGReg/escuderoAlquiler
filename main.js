ba58ac19ce99a4158c5330cbc6704a65fcb006b9
AlvaroGReg <alvaro.gracia.reg@gmail.com>
2023-06-09 13:53:08 +0200

    minor changes

--- main.js
+++ main.js
@@ -1,12 +1,25 @@
-var images = document.getElementsByClassName('images');
+const images = document.getElementsByClassName('images')
+const selectors = document.getElementsByClassName('category')
 
-function showCategory(category) {
+function showCategory(num) {
   for (var i = 0; i < images.length; i++) {
 
-    if (images[i].id === 'images-category-' + category) {
-      images[i].classList.add('active');
+    if (images[i].id === 'images-category-' + num) {
+      images[i].classList.add('active')
     } else {
-      images[i].classList.remove('active');
+      images[i].classList.remove('active')
     }
   }
+
+  activeSelector(num-1)
+}
+
+function activeSelector(num){
+
+  for (var i = 0; i < images.length; i++) {
+
+    selectors[i].classList.remove('active')
+  }
+  
+  selectors[num].classList.add('active')
 }
